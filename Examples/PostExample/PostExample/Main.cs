5a745b506364b8b12ad22554ec52d34394e5c4d6
Peter Gill <peter@majorsilence.com>
2013-12-29 19:44:36 -0330

    Add post example

---
+++ Examples/PostExample/PostExample/Main.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace PostExample
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write (LibCGI.Cgi.Header);
+
+			try {
+				var cgi = new LibCGI.Cgi ();
+				Console.WriteLine ("AuthType: " + cgi.AuthType + "<br>");
+				Console.WriteLine ("ContentLength: " + cgi.ContentLength + "<br>");
+				Console.WriteLine ("ContentType: " + cgi.ContentType + "<br>");
+				Console.WriteLine ("DocumentRoot: " + cgi.DocumentRoot + "<br>");
+				Console.WriteLine ("GatewayInterface: " + cgi.GatewayInterface + "<br>");
+				Console.WriteLine ("HttpAccept: " + cgi.HttpAccept + "<br>");
+				Console.WriteLine ("HttpReferer: " + cgi.HttpReferer + "<br>");
+				Console.WriteLine ("HttpUserAgent: " + cgi.HttpUserAgent + "<br>");
+				Console.WriteLine ("PathInfo: " + cgi.PathInfo + "<br>");
+				Console.WriteLine ("PathTranslated: " + cgi.PathTranslated + "<br>");
+				Console.WriteLine ("QueryString: " + cgi.QueryString + "<br>");
+				Console.WriteLine ("RemoteAddress: " + cgi.RemoteAddress + "<br>");
+				Console.WriteLine ("RemoteHost: " + cgi.RemoteHost + "<br>");
+				Console.WriteLine ("RemoteIdent: " + cgi.RemoteIdent + "<br>");
+				Console.WriteLine ("RemoteUser: " + cgi.RemoteUser + "<br>");
+				Console.WriteLine ("RequestMethod: " + cgi.RequestMethod + "<br>");
+				Console.WriteLine ("ScriptName: " + cgi.ScriptName + "<br>");
+				Console.WriteLine ("ServerName: " + cgi.ServerName + "<br>");
+				Console.WriteLine ("ServerPort: " + cgi.ServerPort + "<br>");
+				Console.WriteLine ("ServerProtocol: " + cgi.ServerProtocol + "<br>");
+				Console.WriteLine ("ServerSoftware: " + cgi.ServerSoftware + "<br>");
+
+				Console.WriteLine ("PostData: " + cgi.PostData + "<br>");
+			}
+			catch (Exception ex) {
+				Console.WriteLine ("Error In CGI<br>");
+				Console.WriteLine ("Message: " + ex.Message + "<br>");
+				Console.WriteLine ("StackTrace: " + ex.StackTrace + "<br>");
+			}
+		}
+	}
+}
